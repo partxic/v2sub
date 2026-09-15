0c2f88fff0c2d87c184177c67827b8b93df59dc6
runoneall <runoobsteve@gmail.com>
2026-09-15 16:30:53 +0800

    feat: update ECH_DNS to include additional DNS queries for improved functionality

--- src/sub.js
+++ src/sub.js
@@ -8,7 +8,7 @@ import pLimit from 'p-limit'
 const limit = pLimit(3)
 
 const ECH_DOMAINS = ['icook.hk', 'cm.edu.kg', 'godotengine.org', 'www.britannica.com', 'lido.fi']
-const ECH_DNS = ['https://dns.alidns.com/dns-query']
+const ECH_DNS = ['https://doh.360.cn/dns-query', 'https://doh.onedns.net/dns-query']
 const INSECURE_PARAMS = ['allowInsecure', 'insecure', 'skip-cert-verify']
 
 const ALL_ECH_PAIRS = []
